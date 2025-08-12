7b8db5b7f0eaf9ffc3b637994a56bf93e1e20b6d
Brian Wishan <bwishan@gmail.com>
2025-08-12 14:28:31 -0700

    Update wt formula to v0.3.0

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -1,9 +1,9 @@
 class Wt < Formula
   desc "Git Worktree Manager - A minimalist CLI for managing git worktrees"
   homepage "https://github.com/bwishan/wt"
-  url "https://github.com/bwishan/wt/releases/download/v0.2.0/wt-0.2.0-universal.tar.gz"
-  sha256 "8a51dbe83b654303481b5c316a2a5edbec2a9bab5a838e0c7f7ab857ef7d4d4d"
-  version "0.2.0"
+  url "https://github.com/bwishan/wt/releases/download/v0.3.0/wt-0.3.0-universal.tar.gz"
+  sha256 "9e0f5db78ba7b88398691fa2c2f7f8216b389041e9fb58e2ae8a3fdb7171b77d"
+  version "0.3.0"
   license "MIT"
 
   depends_on "python@3.11"
@@ -15,6 +15,6 @@ class Wt < Formula
 
   test do
     system "#{bin}/wt", "--version"
-    assert_match "wt 0.2.0", shell_output("#{bin}/wt --version")
+    assert_match "wt 0.3.0", shell_output("#{bin}/wt --version")
   end
 end
